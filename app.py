313e9a542cae00b2f5988ec30d96b57edfd54b6d
noamvaron <noamva96@gmail.com>
2024-06-25 11:30:51 +0300

    chang

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql-hl:5432/furnituredb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@postgresql-hl:5432/furnituredb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
